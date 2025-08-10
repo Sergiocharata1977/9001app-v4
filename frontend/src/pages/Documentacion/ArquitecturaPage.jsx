49d3f6201614572086215ce69eff08cf0d038191
Sergiocharata1977 <69606979+Sergiocharata1977@users.noreply.github.com>
2025-08-10 18:52:35 -0300

    Delete frontend directory

--- frontend/src/pages/Documentacion/ArquitecturaPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EnConstruccion from '../../components/common/EnConstruccion';
-
-const ArquitecturaPage = () => {
-  return <EnConstruccion featureName="Documentación de Arquitectura" />;
-};
-
-export default ArquitecturaPage;
--- frontend/src/pages/IdentificacionProcesosPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import IdentificacionProcesos from '@/components/procesos/IdentificacionProcesos';
-
-const IdentificacionProcesosPage = () => {
-  return <IdentificacionProcesos />;
-};
-
-export default IdentificacionProcesosPage; 
