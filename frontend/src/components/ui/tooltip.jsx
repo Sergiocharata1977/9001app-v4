06619a201e892b5413a9bc96d427c23433956afc
Tu Nombre <tu.email@ejemplo.com>
2025-08-10 14:26:05 -0300

    Remove Frontend directory completely

--- frontend/src/components/ui/tooltip.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Tooltip = ({ content, children }) => (
-  <span title={content}>{children}</span>
-);
-
-export { Tooltip };
-export default Tooltip; 
--- frontend/src/pages/Documentacion/CasosUsoPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EnConstruccion from '../../components/common/EnConstruccion';
-
-const CasosUsoPage = () => {
-  return <EnConstruccion featureName="Casos de Uso" />;
-};
-
-export default CasosUsoPage;
